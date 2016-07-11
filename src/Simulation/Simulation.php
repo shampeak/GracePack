575b7d9bf108c9c77cfe8829c7cac2ef8840f552
shampeak <976107540@qq.com>
2016-07-11 18:05:53 +0800

    0.0007

--- src/Simulation/Simulation.php
+++ src/Simulation/Simulation.php
@@ -6,42 +6,30 @@ use Grace\Base\Base;
 
 class Simulation extends Base
 {
+    private $_config    = array();
+    private $_root      = '';
 
-    public function location($modelname = ''){
-        $rec['model']    = $modelname;
+    public function __construct($config = array()){
+        $this->_config      = $config;
+        $this->_root        = $config['defaultRoot'];
+    }
 
-        //=======================================
-        //对调用时间进行记录
-        //用这个来判断生产环境下是否有调用
-        //=======================================
-
-        $tree = debug_backtrace();
-        array_shift($tree);
-
-        $rec['file']    = current($tree)['file'];
-        $rec['line']    = current($tree)['line'];
-        $rec['_function']= current($tree)['function'];       //根据本类方法判断一些数据类型
-        array_shift($tree);
-        $rec['function'] = current($tree)['function'];
-        $rec['class']    = current($tree)['class'];
-        //OK,获得调用数据
-
-        $rec['hash']    = md5(serialize($rec));             //对自身进行hash
-
-        //=================================================
+    /**
+     * @param $value
+     * 保存对象和对象信息
+     */
+    public function save($rec)
+    {
         $mmcname = 'Simulation.index';
-        /**
-         * 存储
-         * - 判断重复
-         */
+        //=================================================
         $lib = app('mmcfile')->get($mmcname);             //存储到mmcfile里面
         $lib = empty($lib)?array():$lib;
         //判断是否重复
-        D($lib);
         $record = true;
         if(!empty($lib)){
             foreach($lib as $k=>$v){
                 $hash = md5(serialize([
+                    'model'     => $rec['model'],
                     'file'      => $v['file'],
                     'line'      => $v['line'],
                     '_function' => $v['_function'],
@@ -54,10 +42,81 @@ class Simulation extends Base
         //重复性判断结束
         //=================================================
         if($record){
-            $rc[] = $rec;
-            app('mmcfile')->set($mmcname,$rc);//记录
+            $lib[] = $rec;
+            app('mmcfile')->set($mmcname,$lib);//记录
         }
+
     }
+
+    public function location($modelname = ''){
+        $rec['model']    = $modelname;
+        //=======================================
+        //对调用时间进行记录
+        //用这个来判断生产环境下是否有调用
+        //=======================================
+        $tree = debug_backtrace();
+        array_shift($tree);
+        $rec['file']    = current($tree)['file'];
+        $rec['line']    = current($tree)['line'];
+        $rec['_function']= current($tree)['function'];       //根据本类方法判断一些数据类型
+        array_shift($tree);
+        $rec['function'] = current($tree)['function'];
+        $rec['class']    = current($tree)['class'];
+        //OK,获得调用数据
+        $rec['hash']    = md5(serialize($rec));             //对自身进行hash
+        $this->save($rec);      //保存
+    }
+
+
+    /**
+     * @param int    $no
+     * @param string $modelname
+     *
+     * @return array
+     */
+    public function no($no = 0,$modelname = '')
+    {
+    }
+
+    /**
+     * @param int    $int
+     * @param string $modelname
+     *
+     * @return int
+     */
+    public function int($modelname = ''){
+        $this->location($modelname);
+        return
+    }
+
+    /**
+     * @param string $arr
+     * @param string $modelname
+     *
+     * @return string
+     */
+    public function str($arr = '',$modelname = '')
+    {
+        $this->location($modelname);
+        return 'str';
+    }
+
+    /**
+     * @param array  $arr
+     * @param string $modelname
+     *
+     * @return array
+     */
+    public function arr($arr = array(),$modelname = '')
+    {
+        $this->location($modelname);
+        return array();
+    }
+
+
+    //===============================================
+    //===============================================
+    //===============================================
 
     public function test($modelname = ''){
         $this->location($modelname);
